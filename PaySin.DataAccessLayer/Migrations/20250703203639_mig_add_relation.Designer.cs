646df301455ddc1215c2fb4657fef6a5e2038984
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2025-07-03 23:46:03 +0300

    dtos added.

--- PaySin.DataAccessLayer/Migrations/20250703203639_mig_add_relation.Designer.cs
+++ PaySin.DataAccessLayer/Migrations/20250703203639_mig_add_relation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PaySin.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using PaySin.DataAccessLayer.Concrete;
 namespace PaySin.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250703203639_mig_add_relation")]
+    partial class mig_add_relation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

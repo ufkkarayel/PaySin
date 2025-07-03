d0f33e22527147f6baf48539f45ee6655f03f007
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2025-07-03 23:33:58 +0300

    ..

--- PaySin.DataAccessLayer/Migrations/20250703203138_third_mig.Designer.cs
+++ PaySin.DataAccessLayer/Migrations/20250703203138_third_mig.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PaySin.DataAccessLayer.Concrete;
 
@@ -11,9 +12,10 @@ using PaySin.DataAccessLayer.Concrete;
 namespace PaySin.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20250703203138_third_mig")]
+    partial class third_mig
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

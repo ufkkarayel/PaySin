c01271e5169192f6b2f7cc77bf2356270872d3f1
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2025-07-03 23:20:07 +0300

    identitylibrary added.

---
+++ PaySin.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PaySin.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaySin.DataAccessLayer.Concrete
+{
+    public class Context:IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB; initial catalog=PaySinDb; integrated Security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProsess> CustomerAccountProsesses { get; set; }
+    }
+}
---
+++ PaySin.EntityLayer/Concrete/CustomerAccountProsess.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaySin.EntityLayer.Concrete
+{
+    public class CustomerAccountProsess
+    {
+        public int CustomerAccountProsessID { get; set; }
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ProcessDate { get; set; }
+
+    }
+}

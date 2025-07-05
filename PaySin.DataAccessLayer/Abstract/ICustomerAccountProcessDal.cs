da0508383764acec1dae618922642f82fb77172d
ufkkarayel <102018206+ufkkarayel@users.noreply.github.com>
2025-07-05 20:49:21 +0300

    generic repository created

---
+++ PaySin.DataAccessLayer/Abstract/ICustomerAccountProcessDal.cs
@@ -0,0 +1,13 @@
+﻿using PaySin.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaySin.DataAccessLayer.Abstract
+{
+    public interface ICustomerAccountProcessDal:IGenericDal<CustomerAccountProsess>
+    {
+    }
+}
